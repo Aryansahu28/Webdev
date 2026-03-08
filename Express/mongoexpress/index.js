59cf13a9b6ff94d9626a0fc1f0b55332c0ef8ff4
Aryansahu28 <aryansahu.vibranium@gmail.com>
2026-03-08 20:37:56 +0530

    Express mongo

---
+++ Express/mongoexpress/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mongoose from "mongoose"
+import Todo from "./models/Todo.js"
+
+const app = express()
+const port = 3000
+
+let conn = mongoose.connect("mongodb://localhost:27017/Blog");
+
+
+
+
+app.get('/', (req, res) => {
+  const todo = new Todo({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+  })
+  todo.save()
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`) 
+})
