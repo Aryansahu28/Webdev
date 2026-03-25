ef50102b6a5a010b88f8ede3bbc066b8cae8d1b1
Aryansahu28 <aryansahu.vibranium@gmail.com>
2026-03-25 23:18:49 +0530

    Next app router

---
+++ Next/next-intro/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-screen justify-center text-center mt-20'>
+      <button className='bg-amber-500 w-1/3 rounded-3xl hover:font-bold transition-all'>This is about</button>
+    </div>
+  )
+}
+
+export default page
